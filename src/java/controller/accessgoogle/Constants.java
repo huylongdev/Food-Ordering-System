a97edc37276f75718b939336c2df9ffd49c82eb8
Nguyen Hue Suong <suongnhde180706@fpt.edu.vn>
2024-10-06 00:16:04 +0700

    merge file

--- src/java/controller/accessgoogle/Constants.java
+++ src/java/controller/accessgoogle/Constants.java
@@ -13,9 +13,6 @@ public class Constants {
   public static String GOOGLE_LINK_GET_TOKEN = "https://oauth2.googleapis.com/token";
   public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
   public static String GOOGLE_GRANT_TYPE = "authorization_code";
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 }
   
