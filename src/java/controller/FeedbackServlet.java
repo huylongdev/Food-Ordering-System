17693b73906460198da8a778149e3370ba50a2e8
gianglamnguyen <133856538+gianglamnguyen@users.noreply.github.com>
2024-10-17 00:35:35 +0700

    CRUD feedback
    Chưa làm css, chưa edit, lỗi nút bấm edit,delete. Đã hoàn RCD feedback

---
+++ src/java/controller/FeedbackServlet.java
@@ -0,0 +1,137 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import context.FeedbackDAO;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import model.Account;
+import model.Feedback;
+
+/**
+ *
+ * @author giang
+ */
+public class FeedbackServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FeedbackServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet FeedbackServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        switch(action){
+            case "add" -> addFeedback(request, response);
+            case "edit" -> editFeedback(request, response);
+            case "delete" -> deleteFeedback(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    
+    private void addFeedback(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        Account user = (Account) request.getSession().getAttribute("user");
+        int id = Integer.parseInt(request.getParameter("productID"));
+        String comment = request.getParameter("comment");
+        int rating = Integer.parseInt(request.getParameter("rating"));
+        FeedbackDAO fdao = new FeedbackDAO();
+        Feedback f = new Feedback();
+        f.setComment(comment);
+        f.setFeedbackId(id);
+        f.setUserId(user.getUserID());
+        f.setRating(rating);
+        fdao.addFeedback(f);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/food-detail?productId=" + id);
+        dispatcher.forward(request, response);
+    }
+    private void editFeedback(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        Account user = (Account) request.getSession().getAttribute("user");
+        int id = Integer.parseInt(request.getParameter("productID"));
+        String comment = request.getParameter("comment");
+        int rating = Integer.parseInt(request.getParameter("rating"));
+        FeedbackDAO fdao = new FeedbackDAO();
+        Feedback f = new Feedback();
+        f.setComment(comment);
+        f.setFeedbackId(id);
+        f.setUserId(user.getUserID());
+        f.setRating(rating);
+        fdao.addFeedback(f);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/food-detail?productId=" + id);
+        dispatcher.forward(request, response);
+    }
+    private void deleteFeedback(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        int fid = Integer.parseInt(request.getParameter("feebackID"));
+        int id = Integer.parseInt(request.getParameter("productID"));
+        FeedbackDAO fdao = new FeedbackDAO();
+        fdao.deleteFeedback(fid);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/food-detail?productId=" + id);
+        dispatcher.forward(request, response);
+    }
+    
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/model/FeedbackDTO.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author giang
+ */
+public class FeedbackDTO {
+    private int feedbackId, productId, rating;
+    private String comment, userName;
+    private Date createdDate;
+
+    public FeedbackDTO(int feedbackId, int productId, int rating, String comment, String userName, Date createdDate) {
+        this.feedbackId = feedbackId;
+        this.productId = productId;
+        this.rating = rating;
+        this.comment = comment;
+        this.userName = userName;
+        this.createdDate = createdDate;
+    }
+
+    public int getFeedbackId() {
+        return feedbackId;
+    }
+
+    public void setFeedbackId(int feedbackId) {
+        this.feedbackId = feedbackId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+    
+    
+}
