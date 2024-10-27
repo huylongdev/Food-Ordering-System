ee4d6944ef23ec5405f8647e9885a8c9d8c6e669
Quoc Huy <huydqds180257@fpt.edu.vn>
2024-10-27 12:52:29 +0700

    Re-add Constants.java without sensitive information

---
+++ src/java/controller/accessgoogle/Constants.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.accessgoogle;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Constants {
+
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/OrderingSystem/login-google";
+    public static String GOOGLE_LINK_GET_TOKEN = "https://oauth2.googleapis.com/token";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
